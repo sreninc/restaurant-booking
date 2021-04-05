316e0efb221e9dab64ac4a2f51922aec0fd587e7
sreninc <sreninc@gmail.com>
2021-04-05 08:23:23 +0000

    STYLE: Added style to client page turning into table

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
