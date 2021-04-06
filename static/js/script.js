0e3657f1e383199c3cd5ae2f9116b5f5d1e50c35
sreninc <sreninc@gmail.com>
2021-04-06 19:31:20 +0000

    FEAT: Added script and html to edit client and populate modal

--- static/js/script.js
+++ static/js/script.js
@@ -31,4 +31,22 @@ function editClient(clientId) {
     var elem = document.querySelectorAll('.modal')[0];
     var instance = M.Modal.getInstance(elem);
     instance.open();
+
+    const row = document.getElementById(clientId);
+    var firstName = row.nextElementSibling;
+    var lastName = firstName.nextElementSibling;
+    var email = lastName.nextElementSibling;
+    var mobile = email.nextElementSibling;
+    
+    firstName = firstName.innerHTML.trim();
+    lastName = lastName.innerHTML.trim();
+    email = email.innerHTML.trim();
+    mobile = mobile.innerHTML.trim();
+
+    document.getElementById("firstName").value = firstName;
+    document.getElementById("lastName").value = lastName;
+    document.getElementById("email").value = email;
+    document.getElementById("mobile").value = mobile;
+
+    M.updateTextFields();
 }
