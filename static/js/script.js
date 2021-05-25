b78aa7bfbb7c0bd3e73deba2b25ed006e1b10639
sreninc <sreninc@gmail.com>
2021-05-25 19:51:35 +0000

    FEAT: Added confirm modal to delete guest in guest details

--- static/js/script.js
+++ static/js/script.js
@@ -67,3 +67,19 @@ function confirmDeleteBooking(bookingId, clientId, value, status) {
         }
     })
 }
+
+function confirmDeleteGuest(clientId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href="/delete_client/" + clientId;
+        }
+    })
+}
